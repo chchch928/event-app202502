05843429e039712eeec598e9eb7deab9ef386c0d
chchch928 <kbanda@naver.com>
2025-02-17 17:07:50 +0900

    #2_ 서버에서 이벤트 목록 가져오기

--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -1,17 +1,36 @@
-import MainNavigation from "../layout/MainNavigation";
+import { useEffect, useState } from "react";
 
 const EventsPage = () => {
 
-    return (
-      <>
-    
+  const [eventList, setEventList] = useState([]);
+
+  useEffect(() => { 
+
+    const fetchEvents = async () => { 
+      const response = await fetch(`http://localhost:9000/api/events`);
+      const responseData = await response.json();
+
+      setEventList(responseData);
+    };
+
+    fetchEvents();
+
+  }, []);
+
+
+  return (
+    <>
       <h1>Events Page</h1>
-        <ul>
-        </ul>
-     
-        
-      </>
-    );
-  };
-  
-  export default EventsPage;
+      <ul>
+        {
+          eventList.map(ev => (
+            <li key={ev.id}>{ ev.title }</li>
+          ))
+        }
+      </ul>
+      
+    </>
+  );
+};
+
+export default EventsPage;
