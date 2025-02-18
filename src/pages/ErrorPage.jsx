dd2a4f2633c051a59084e9233a57ff9aae43a46c
chchch928 <kbanda@naver.com>
2025-02-18 11:31:39 +0900

    #9_서버에서 보낸 에러메세지 에러페이지에서  처리하기

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import MainNavigation from '../layout/MainNavigation';
+import { useRouteError } from 'react-router-dom';
 
 const ErrorPage = () => {
+
+  // loader가 throw한 에러정보를 받아서 사용하기
+  const error = useRouteError();
+  console.log(error);
+
+  let errorText =`페이지를 찾을 수 없습니다. URL을 확인하세요`;
+
+  if(error.status != 404){
+    errorText = JSON.parse(error.data).message;
+  }
+  
+
   return (
     <>
       <MainNavigation />
       <main>
         <h1>페이지를 찾을 수 없습니다!</h1>
-        <p>요청하신 페이지의 주소를 다시 확인해주세요.</p>
+        <p>{errorText}</p>
       </main>
     </>
   );
--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -37,7 +37,17 @@ export const loader = async () =>{
   const res = await fetch(`http://localhost:9000/api/events`);
   // const jsonData = await res.json();
   
-  //console.log(jsonData);
+  //예외처리 
+  if(!res.ok){
+    const jsonData = await res.json();
+
+    throw new Response(
+      JSON.stringify(jsonData), // 에러메세지지
+      {
+        status : res.status
+      } // 에러정보 객체
+    );
+  }
 
   // loader가 리턴한 데이터는 라우팅된 페이지와 
   // 그 컴포넌트의 하위 컴포넌트에서 언제든 뽑아서 사용할 수 있음
