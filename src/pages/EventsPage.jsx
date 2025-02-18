91a3c02c06d38c80a8ca6f35870115ac61388b18
chchch928 <kbanda@naver.com>
2025-02-18 11:17:23 +0900

    #8_event loader 아웃소싱

--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -8,7 +8,7 @@ const EventsPage = () => {
 
   // const [eventList, setEventList] = useState([]);
 
-  // useEffect는 렌더링 이후에 실행됨됨
+  // useEffect는 렌더링 이후에 실행됨
   // useEffect(() => { 
 
   //   const fetchEvents = async () => { 
@@ -31,3 +31,16 @@ const EventsPage = () => {
 };
 
 export default EventsPage;
+
+// loader를 아웃소싱
+export const loader = async () =>{
+  const res = await fetch(`http://localhost:9000/api/events`);
+  // const jsonData = await res.json();
+  
+  //console.log(jsonData);
+
+  // loader가 리턴한 데이터는 라우팅된 페이지와 
+  // 그 컴포넌트의 하위 컴포넌트에서 언제든 뽑아서 사용할 수 있음
+  return res; // loader는 fetch 결과를 바로 리턴하는 경우 json 추출작업을 자동수행
+
+};
--- src/routes/route-config.jsx
+++ src/routes/route-config.jsx
@@ -1,10 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import ErrorPage from "../pages/ErrorPage";
 import HomePage from "../pages/HomePage";
-import EventsPage from "../pages/EventsPage";
+import EventsPage,{loader as eventsLoader } from "../pages/EventsPage";
 import RootLayout from "../layout/rootLayout";
 import EventDetailPage from "../pages/EventDetailPage";
 import EventLayout from "../layout/EventLayout";
+
 
 const router = createBrowserRouter([
   {
@@ -24,19 +25,8 @@ const router = createBrowserRouter([
           {
             index: true,
             element: <EventsPage/>,
-            // loader 함수는 이 페이지가 라우팅될때 자동으로 트리거되는 함수수
-            loader: async() => {
-              const res = await fetch(`http://localhost:9000/api/events`);
-              const jsonData = await res.json();
-              
-              //console.log(jsonData);
-
-              // loader가 리턴한 데이터는 라우팅된 페이지와 
-              // 그 컴포넌트의 하위 컴포넌트에서 언제든 뽑아서 사용할 수 있음음
-              return jsonData;
-              
-            }
-              
+            // loader 함수는 이 페이지가 라우팅될때 자동으로 트리거되는 함수
+            loader: eventsLoader,
               
           },
           {
