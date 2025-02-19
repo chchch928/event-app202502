e16de50e37ac5b79afb1af649ab9147c388c2ac7
chchch928 <kbanda@naver.com>
2025-02-19 11:02:59 +0900

    #18_액션함수로 삭제요청 보내기기

--- src/routes/route-config.jsx
+++ src/routes/route-config.jsx
@@ -3,7 +3,7 @@ import ErrorPage from "../pages/ErrorPage";
 import HomePage from "../pages/HomePage";
 import EventsPage,{loader as eventsLoader } from "../pages/EventsPage";
 import RootLayout from "../layout/rootLayout";
-import EventDetailPage, {loader as eventDetailLoader} from "../pages/EventDetailPage";
+import EventDetailPage, {loader as eventDetailLoader, deleteAction} from "../pages/EventDetailPage";
 import EventLayout from "../layout/EventLayout";
 import NewEventPage, {action as mainpulateAction} from "../pages/NewEventPage";
 import EditPage from '../pages/EditPage';
@@ -40,6 +40,7 @@ const router = createBrowserRouter([
             path: ':eventId',
             element: <EventDetailPage />,
             loader: eventDetailLoader,
+            action: deleteAction
           },
           {
             path: ':eventId/edit',
