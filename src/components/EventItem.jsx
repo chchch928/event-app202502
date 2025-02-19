eb7bb10eda1b532985bba3757858733bd0b94ccb
chchch928 <kbanda@naver.com>
2025-02-19 10:40:38 +0900

    #17_삭제이벤트 처리

--- src/components/EventItem.jsx
+++ src/components/EventItem.jsx
@@ -1,13 +1,30 @@
+import { Link, useNavigate } from 'react-router-dom';
 import styles from './EventItem.module.scss';
-import {Link} from 'react-router-dom';
 
 const EventItem = ({ event }) => {
+
+  const navigate = useNavigate();
+  
   const {
+    id,
     title,
     desc: description,
     'img-url': image,
     'start-date': date,
   } = event;
+
+
+  // 삭제 처리 이벤트
+  const handleRemove = e => { 
+
+    if (!confirm('정말 삭제하시겠습니까?')) return;
+
+    (async () => { 
+      const res = await fetch(`http://localhost:9000/api/events/${id}`, { method: 'DELETE' });
+      if (res.ok) navigate('/events');
+    })();
+
+  };
 
   return (
     <article className={styles.event}>
@@ -19,8 +36,8 @@ const EventItem = ({ event }) => {
       <time>{date}</time>
       <p>{description}</p>
       <menu className={styles.actions}>
-      <Link to='edit'>Edit</Link>
-        <button>Delete</button>
+        <Link to='edit'>Edit</Link>
+        <button onClick={handleRemove}>Delete</button>
       </menu>
     </article>
   );
